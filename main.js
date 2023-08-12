ebab4b94106234c3dc99fae7d8e14e132616f7c3
Chris <c.havrez@stag.technofuturtic.education>
2023-08-13 01:54:21 +0200

    update JavaScript

---
+++ main.js
@@ -0,0 +1,75 @@
+
+//1================== Selection (id) Des Bouton !====================
+let listeBtn1 = document.getElementById("btn1")
+let listeBtn2 = document.getElementById("btn2")
+let listeBtn3 = document.getElementById("btn3")
+let listeBtn4 = document.getElementById("btn4")
+
+//2================== Selection (id) Affichage !====================
+let NomAffichage = document.getElementById("NomAffichage")
+let QuantiterAffichage = document.getElementById("QuantiterAffichage")
+let PrixAffichage = document.getElementById("PrixAffichage")
+let RuptureAffichage = document.getElementById("RuptureAffichage")
+// =========================Selection (id) Affichage !=================
+
+
+// ===================Fonction a l evenements==================
+listeBtn1.addEventListener("click", function () {
+// =========================affichage elements stock=================
+NomAffichage.innerText = (`Boisson :${Coca.nom}`)
+QuantiterAffichage.innerText=(`Quantiter:${Coca.quantiter}`)
+PrixAffichage.innerText=(`Prix :${Coca.prix} euro`)
+// =========================affichage elements stock=================
+    if (Coca.quantiter) {
+        Coca.quantiter -= 1
+    }
+    else {
+        RuptureAffichage.innerText=(`Le Stock de ${Coca.nom} est de ${Coca.rupture}`)
+    }
+})
+listeBtn2.addEventListener("click", function () {
+// =========================affichage elements stock=================
+NomAffichage.innerText = (`Boisson :${Fanta.nom}`)
+QuantiterAffichage.innerText=(`Quantiter:${Fanta.quantiter}`)
+PrixAffichage.innerText=(`Prix :${Fanta.prix} euro`)
+// =========================affichage elements stock=================
+    if (Fanta.quantiter) {
+        Fanta.quantiter -= 1
+    }
+    else {
+        RuptureAffichage.innerText=(`Le Stock de ${Fanta} est de ${Fanta.rupture}`)
+    }
+})
+listeBtn3.addEventListener("click", function () {
+
+
+    // =========================affichage elements stock=================
+NomAffichage.innerText = (`Boisson :${Sprit.nom}`)
+QuantiterAffichage.innerText=(`Quantiter:${Sprit.quantiter}`)
+PrixAffichage.innerText=(`Prix :${Sprit.prix} euro`)
+// =========================affichage elements stock=================
+    if (Sprit.quantiter) {
+        Sprit.quantiter -= 1
+    }
+    else {
+        RuptureAffichage.innerText=(`Le Stock de ${Sprit} est de ${Sprit.rupture}`)
+    }
+})
+listeBtn4.addEventListener("click", function () {
+// =========================affichage elements stock=================
+    NomAffichage.innerText = (`Boisson :${Eau.nom}`)
+    QuantiterAffichage.innerText=(`Quantiter:${Eau.quantiter}`)
+    PrixAffichage.innerText=(`Prix :${Eau.prix} euro`)
+// =========================affichage elements stock=================
+    if (Eau.quantiter > 0) {
+        Eau.quantiter -= 1
+    }
+    else {
+        RuptureAffichage.innerText=(`Le Stock de ${Sprit} est de ${Sprit.rupture}`)
+    }
+
+})
+
+// les moddif a faire dans le tableau remplacer et adapter rupture a false
+// description machine en injectant du js dans le html !
+
---
+++ tableau.js
@@ -0,0 +1,28 @@
+// ======================stock========================
+
+let Coca = {
+    nom: "Coca",
+    quantiter: 10,
+    prix: 1,
+    rupture:0
+}
+let Fanta = {
+    nom: "Fanta",
+    quantiter: 10,
+    prix: 1,
+    rupture:0
+}
+let Sprit = {
+    nom: "Sprit",
+    quantiter: 10,
+    prix: 1,
+    rupture:0
+
+}
+let Eau = {
+    nom: "Eau",
+    quantiter: 10,
+    prix: 1,
+    rupture:0
+
+}
